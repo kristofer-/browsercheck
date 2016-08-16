62bae07f395974f7b680416021161fdbb6dbe3be
Kristofer Månsson <kristofer.mansson@gmail.com>
2016-08-16 14:25:08 +0200

    Updated so all is bolean

--- browserdetection.js
+++ browserdetection.js
@@ -6,7 +6,7 @@ window.browser.is_ie = navigator.userAgent.indexOf('MSIE') > -1;
 window.browser.is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
 window.browser.is_safari = navigator.userAgent.indexOf("Safari") > -1;
 window.browser.is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
-window.browser.is_ios = navigator.userAgent.toLowerCase().match(/(ipad|iphone)/);
+window.browser.is_ios = (navigator.userAgent.toLowerCase().match(/(ipad|iphone)/))?true:false;
 
 if ((window.browser.is_chrome) && (window.browser.is_safari)) {
     window.browser.is_safari = false;
