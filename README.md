d7c29c3d5e25f31b24fa8728d4079363f89b45e9
Kristofer Månsson <kristofer.mansson@gmail.com>
2016-08-16 11:59:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+#Browser detection
+Detects which useragent is used.
+#Detected browsers
+- Chrome
+- MSIE (Internet explorer)
+- Firefox
+- Safari
+- Safari for iOS
+- Opera
---
+++ browserdetection.js
@@ -0,0 +1,16 @@
+/*jshint browser: true*/
+/* Detects which useragent is used */
+window.browser = (window.browser) ? window.browser : {};
+window.browser.is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+window.browser.is_ie = navigator.userAgent.indexOf('MSIE') > -1;
+window.browser.is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+window.browser.is_safari = navigator.userAgent.indexOf("Safari") > -1;
+window.browser.is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+window.browser.is_ios = navigator.userAgent.toLowerCase().match(/(ipad|iphone)/);
+
+if ((window.browser.is_chrome) && (window.browser.is_safari)) {
+    window.browser.is_safari = false;
+}
+if ((window.browser.is_chrome) && (window.browser.is_opera)) {
+    window.browser.is_chrome = false;
+}
